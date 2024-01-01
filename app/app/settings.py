d6b923a3ddbb4525620ed8673f6849e48826d147
Matt Seidel <mseidel819@gmail.com>
2024-01-01 11:30:32 -0600

    token tests

--- app/app/settings.py
+++ app/app/settings.py
@@ -39,8 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authentication',
     'drf_spectacular',
     "core",
+    "user",
 ]
 
 MIDDLEWARE = [
--- app/app/urls.py
+++ app/app/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from drf_spectacular.views import (
     SpectacularAPIView, SpectacularSwaggerView
     )
@@ -25,4 +25,5 @@ urlpatterns = [
          name='schema'),  # yaml file
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'),
          name='api-docs'),
+    path('api/user/', include('user.urls')),
 ]
