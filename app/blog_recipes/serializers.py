655ef867fb5c41afb76bcf913462f674b288c144
Matt Seidel <mseidel819@gmail.com>
2024-01-11 12:02:29 -0600

    refactor nested ser to only show text str

--- app/blog_recipes/serializers.py
+++ app/blog_recipes/serializers.py
@@ -16,31 +16,44 @@ from core.models import (
 class BlogIngredientSerializer(serializers.ModelSerializer):
     """Serializer for ingredient object"""
     class Meta:
+        """Meta class"""
         model = BlogIngredient
-        fields = ('id', 'ingredient',)
-        read_only_fields = ('id',)
+        fields = ('ingredient',)
 
 
 class BlogNoteSerializer(serializers.ModelSerializer):
     """Serializer for note object"""
     class Meta:
+        """Meta class"""
         model = BlogNote
-        fields = ('id', 'note')
-        read_only_fields = ('id',)
+        fields = ('note',)
 
 
 class BlogInstructionSerializer(serializers.ModelSerializer):
     """Serializer for instruction object"""
     class Meta:
+        """Meta class"""
         model = BlogInstruction
-        fields = ('id', 'instruction')
+        fields = ('instruction',)
+
+
+class BlogAuthorSerializer(serializers.ModelSerializer):
+    """Serializer for author object"""
+    # recipes = BlogRecipeSerializer(many=True, read_only=True)
+
+    class Meta:
+        """Meta class"""
+        model = BlogAuthor
+        fields = ('id', 'name', 'website_link')
         read_only_fields = ('id',)
 
 
 class BlogRecipeSerializer(serializers.ModelSerializer):
     """Serializer for recipes"""
+    author = BlogAuthorSerializer(read_only=True)
 
     class Meta:
+        """Meta class"""
         model = BlogRecipe
         fields = (
             "id", "title", "link", "rating", "category", "author", "slug",
@@ -52,11 +65,12 @@ class BlogRecipeDetailSerializer(BlogRecipeSerializer):
     """
     Serializer for recipe detail object
     """
-    ingredients = BlogIngredientSerializer(many=True, read_only=True)
-    instructions = BlogInstructionSerializer(many=True, read_only=True)
-    notes = BlogNoteSerializer(many=True, read_only=True)
+    ingredients = serializers.SerializerMethodField()
+    instructions = serializers.SerializerMethodField()
+    notes = serializers.SerializerMethodField()
 
     class Meta(BlogRecipeSerializer.Meta):
+        """Meta class"""
         fields = BlogRecipeSerializer.Meta.fields + (
             "description", "num_reviews", "prep_time", "cook_time",
             "total_time", "servings", "ingredients", "instructions", "notes"
@@ -64,12 +78,19 @@ class BlogRecipeDetailSerializer(BlogRecipeSerializer):
 
         read_only_fields = ('id',)
 
+    def get_ingredients(self, obj):
+        """orders ingredients by id to preserve original order"""
+        ordered_ingredients = obj.ingredients.order_by('id')
+        return [ingredient['ingredient']
+                for ingredient in ordered_ingredients.values()]
 
-class BlogAuthorSerializer(serializers.ModelSerializer):
-    """Serializer for author object"""
-    recipes = BlogRecipeSerializer(many=True, read_only=True)
+    def get_instructions(self, obj):
+        """orders instructions by id to preserve original order"""
+        ordered_instructions = obj.ingredients.order_by('id')
+        return [instruction['instruction']
+                for instruction in ordered_instructions.values()]
 
-    class Meta:
-        model = BlogAuthor
-        fields = ('id', 'name', 'website_link', "recipes")
-        read_only_fields = ('id',)
+    def get_notes(self, obj):
+        """orders notes by id to preserve original order"""
+        ordered_notes = obj.notes.order_by('id')
+        return [note['note'] for note in ordered_notes.values()]
