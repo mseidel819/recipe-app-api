78db79a76d948328d1b06a9e632d7e44aed99912
Matt Seidel <mseidel819@gmail.com>
2024-01-23 13:42:13 -0600

    page imit

--- app/blog_recipes/views.py
+++ app/blog_recipes/views.py
@@ -27,7 +27,7 @@ from blog_recipes import serializers
 
 
 class CustomPageNumberPagination(PageNumberPagination):
-    page_size = 10  # Set your desired page size
+    page_size = 15  # Set your desired page size
     page_size_query_param = 'page_size'
     max_page_size = 50
 
