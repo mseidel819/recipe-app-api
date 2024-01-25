0a65a8e97fd87506ad7d440c5fa832cb0d03cf8e
Matt Seidel <mseidel819@gmail.com>
2024-01-25 15:35:38 -0600

    flake

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -4,10 +4,10 @@ tests for user
 
 from django.test import TestCase
 from django.contrib.auth import get_user_model
-from django.urls import reverse
+# from django.urls import reverse
 
 from rest_framework.test import APIClient
-from rest_framework import status
+# from rest_framework import status
 
 
 # CREATE_USER_URL = reverse("user:create")
